b5d1d1e15acc42578917881ec4d2d7031b65421c
bhagi25 <bhagavathikondapally@gmail.com>
2018-03-19 16:06:38 -0500

    file modified by div

--- exam.php
+++ exam.php
@@ -15,4 +15,17 @@ add(9,80);
 echo "<br>";
 minus(6,1);
 
+function multiply($x,$y)
+{
+	echo $x*$y;
+
+}
+echo <"br>";
+multiply(5,6);
+function divide($x,$y)
+{
+    echo $x/$y;
+}
+divide(10,2);
+
 ?>
